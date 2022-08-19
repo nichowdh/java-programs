bf713ef1ebafcaa6b3523703a74a329c32d346dc
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 10:55:37 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class PositiveNumber {
+	
+	public static void main(String args[])
+	{
+		int num=6;
+		String result=num>=5?"positive number":"negetive number";
+		System.out.println(result);
+		
+	}
+
+}
