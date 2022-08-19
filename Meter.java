1b1d4460a2d2375325ccb487a9a3bd9239fb5342
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 16:03:45 +0530

    Add files via upload

---
+++ Meter.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class Meter {
+	public static void main(String args[])
+	{
+		double inches= 1000;
+		double Meter=inches*0.0254;
+		
+		System.out.println("value in Merer is: "+Meter);
+		
+	}
+		
+	
+	
+
+}
