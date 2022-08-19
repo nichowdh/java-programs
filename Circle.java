b6ab50d22826ace59704fe02e93fa0ff7e18666a
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 15:53:18 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Circle {
+	public static void main(String[] args)
+	{
+		double r; //r = radius of the circle
+		double pi=3.142, area;
+		r= 10;
+		area = pi*r*r;
+		System.out.println("Area of Circle is: "+area);
+	}
+
+}
