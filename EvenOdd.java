d773c1e7ad9dc2ca750ea9d4cee719d027c3413f
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 16:51:58 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+package com.java; //Write a Java Program to find whether a number is even or odd using conditional operator
+
+public class EvenOdd {
+	public static void main(String args[])
+	{
+		int a=7;
+		if (a%2==0) {
+			System.out.println("Number is Even ");
+		}
+		else {
+			System.out.println("Number is odd ");
+		}
+	}
+
+}
