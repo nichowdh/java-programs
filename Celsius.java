f15f70a6dc3daf0d47009a06ac9a080169a04c38
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 10:48:26 +0530

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Celsius {
+	public static void main(String args[])
+	{
+		float F, C;
+		
+		F=102;
+		C=((F-32)*5)/9;
+		
+		System.out.println("Temperature in Celsius: "+C);
+	}
+
+}
