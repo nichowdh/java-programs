f93ba66bd983b0b59bbdcf02baa1abcd013881a4
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 16:05:21 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Swap {
+	public static void main(String args[])
+	
+	{
+		int a=5,b=6,temp;
+		System.out.println("Before swap ");	
+		System.out.println(a);
+		System.out.println(b);
+
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swap ");	
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
