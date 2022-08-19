7d5870a14e16bdcadfedc1c8d4d0f14f19a0c8f6
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-08-19 16:02:49 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class LargestNumber {
+	public static void main(String args[])
+	{
+		int num1=20;
+		  int num2=12;
+		  if(num1>num2 && num1<num2) 
+		  {
+			  System.out.println("num1 is gretest");
+			  
+		  }
+		  else 
+		  {
+			  System.out.println("num2 is gretest");
+		  }
+	}
+
+}
