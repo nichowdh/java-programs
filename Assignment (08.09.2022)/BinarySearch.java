c58cd0ebf4a95fb2b4c747cf853c8e2fb7742b6c
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-09-08 13:56:07 +0530

    Add files via upload

---
+++ Assignment (08.09.2022)/BinarySearch.java
@@ -0,0 +1,29 @@
+package arr;
+
+public class BinarySearch {
+	public static int binarySearch(int arr[], int low, int high, int key){  
+        if (high>=low){  
+            int mid = low + (high - low)/2;  
+            if (arr[mid] == key){  
+            return mid;  
+            }  
+            if (arr[mid] > key){  
+            return binarySearch(arr, low, mid-1, key);  
+            }else{  
+            return binarySearch(arr, mid+1, high, key);
+            }  
+        }  
+        return -1;  
+    }  
+    public static void main(String args[]){  
+        int arr[] = {10,20,30,40,50,60};  
+        int key = 70;  
+        int high=arr.length-1;  
+        int result = binarySearch(arr,0,high,key);  
+        if (result == -1)  
+            System.out.println("Element is not found!");  
+        else  
+            System.out.println("Element is found at index: "+result);  
+    }  
+
+}
