3239bf010c4f048e443af86e7908771bd5d031da
nichowdh <111556280+nichowdh@users.noreply.github.com>
2022-09-22 12:59:29 +0530

    Add files via upload

---
+++ Assignment (22.09.2022)/Car.java
@@ -0,0 +1,48 @@
+class Car{
+//default Constructor
+//constructor name should be same as class name
+   public Car()
+   {
+	System.out.println("Class Car");
+   }
+   public void vehicleType()
+   {
+	System.out.println("Vehicle Type: Car");
+   }
+}
+
+class Maruti extends Car{
+//default constructor
+   public Maruti()
+   {
+	System.out.println("Class Maruti");
+   }
+   public void brand()
+   {
+	System.out.println("Brand: Maruti");
+   }
+   public void speed()
+   {
+	System.out.println("Max: 90Kmph");
+   }
+}
+
+ class Maruti800 extends Maruti{
+//default constructor
+
+   public Maruti800()
+   {
+	System.out.println("Maruti Model: 800");
+}
+ public void speed()
+   {
+	System.out.println("Max: 80Kmph");
+   }
+   public static void main(String args[])
+   {
+	 Maruti800 obj=new Maruti800();
+	 obj.vehicleType();
+	 obj.brand();
+	 obj.speed();
+   }
+}
